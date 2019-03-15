decd3ee61f690eca04c500b57191cfe194902109
Aryan Sharma <asharma00@stuy.edu>
2019-03-15 10:12:07 -0400

    fw

---
+++ Merge.java
@@ -0,0 +1,18 @@
+public class Merge {
+
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data){
+
+
+  }
+
+  //pseudo
+  //mergesort(data,lo,hi):
+  //if lo >= hi :
+    //return
+  //mergesort left side
+  //mergesort right side
+  //merge
+
+
+}
