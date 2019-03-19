23a787107d4fa213d653a6dc97cda90dd8191b43
Aryan Sharma <asharma00@stuy.edu>
2019-03-18 22:26:44 -0400

    comments

--- Merge.java
+++ Merge.java
@@ -2,20 +2,20 @@ public class Merge {
 
   /*sort the data from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[] data) {
-       if (data.length > 1) {
-           // split data into two halves
-           int mid = data.length / 2;
-           int[] left = new int[mid];
+       if (data.length > 1) { //go until its lenght is 1
+           // splits data into two halves
+           int mid = data.length / 2; //middle index
+           int[] left = new int[mid]; //make left array with all the values from start to mid
            for (int i = 0; i < mid; i++) {
                left[i] = data[i];
            }
-           int[] right = new int[data.length-mid];
+           int[] right = new int[data.length-mid]; //make right array with all values from mid to end
            for (int i = 0; i < data.length-mid; i++) {
                right[i] = data[i + mid];
            }
-           mergesort(left);
-           mergesort(right);
-           merge(data, left, right);
+           mergesort(left);//recursively call wrapper on left side
+           mergesort(right);//recursivley call weapper on right side
+           merge(data, left, right); //merge both using helper
        }
    }
 
